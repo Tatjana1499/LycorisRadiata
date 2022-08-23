b05ad0da6c33b29afb0dab59dd485fdecd1e2fd3
Tatjana1499 <bailovict@gmail.com>
2022-08-23 18:40:10 +0200

    Dodaje interfejse repozitorijuma

---
+++ SlojPristupaPodacima/Interfejsi/IRepozitorijum.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SlojPristupaPodacima
+{
+    public interface IRepozitorijum<TEntitet> where TEntitet : class
+    {
+        public void Dodaj(TEntitet entitet );
+        public void Brisi(TEntitet entitet);
+        public List<TEntitet> Pretraga(Func<TEntitet, bool> uslov);
+        public List<TEntitet> VratiSve();
+        public void Azuriraj(TEntitet entitet);
+
+    }
+}
